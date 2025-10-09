b12ec0534f85e70bf46b704845d68a60f0723fca
p0larisrip <50590525ddd@gmail.com>
2025-10-09 11:55:03 +0300

    Update lab3_1.cpp

--- lab3_1/lab3_1/lab3_1.cpp
+++ lab3_1/lab3_1/lab3_1.cpp
@@ -6,16 +6,16 @@ int main()
 {   
     int mper = 42;
     cout << "start: " << mper << endl;
-    int* ptr = &mper;
+    int* ptr = &mper; //запрос адреса и значения
     cout << "adres: " << ptr << "\nznachenie: " << *ptr << endl;
     int a[] = { 1, 2, 3, 4, 5 };
-    *ptr = 20;
+    *ptr = 20; //изменение значения на адресе
     cout << "posle izmenenia: " << mper << endl;
     cout << "massiv: ";
     int* apt = a;
     for (int i = 0; i < 5; i++)
     {
-        cout << *(apt + i) << "";
+        cout << *(apt + i) << ""; //вывод через указатель
     }
     cout << endl;
     int x = 5;
@@ -23,6 +23,8 @@ int main()
     int* const bpt = &x;
     *bpt = 15;
     cout << "x izmenen na: " << x << endl;
+    //bpt = &mper ---- не работает
     return 0;
 }
+
 
