1e2b8a231345866d5653b60eae2f543e73661c43
p0larisrip <50590525ddd@gmail.com>
2025-10-14 13:40:01 +0300

    Add files via upload

---
+++ Lab 1_2/Lab 1_2/Lab 1_2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string> 
+using namespace std;
+
+int main()
+{
+    int a = 150;
+    float b = 15.933f;
+    unsigned char c = 250;
+    cout << "a = " << a << "\nb = " << b << "\nc = " << int(c) << endl;
+
+    int d = 3;  
+    string m = "november";
+    int y = 2006;
+    cout << "MBDay: " << d << " " << m << " " << y << " year" << endl;  
+
+    const float num = 2.3f;
+    const string os = "WINDOWS";
+    cout << num << " " << os << endl;
+
+    return 0;
+}
