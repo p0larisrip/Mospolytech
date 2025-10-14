c73d1c8d868e8629fbc248a82b3f316f487bb54e
p0larisrip <50590525ddd@gmail.com>
2025-10-14 13:19:33 +0300

    Add files via upload

---
+++ Lab1_1/Lab1_1/Lab1_1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "First string" << endl;
+    cout << "Первая строка\nВторая строка" << endl;
+    setlocale(LC_ALL, "Russian");
+    cout << "Проверка русских символов" << endl;
+    cout << "Спецсимволы \"\\\" " << endl;
+    return 0;
+} 
