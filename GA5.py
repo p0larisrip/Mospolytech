25e56d99cbef105c3cde61778e9863a7d4d98832
p0larisrip <50590525ddd@gmail.com>
2025-10-27 15:17:23 +0300

    Add files via upload

---
+++ GA5.py
@@ -0,0 +1,43 @@
+from collections import deque
+graf = {}
+graf['S'] = ['A', 'B', 'E', 'D']
+graf['A'] = ['S']
+graf['B'] = ['S', 'C', 'D']
+graf['C'] = ['B', 'J']
+graf['D'] = ['B', 'G', 'S']
+graf['E'] = ['S', 'G']
+graf['F'] = ['G', 'H']
+graf['G'] = ['E', 'D', 'F', 'H', 'J']
+graf['H'] = ['F', 'G', 'I']
+graf['I'] = ['H', 'J']
+graf['J'] = ['I' , 'G', 'C']
+def shortest(st, end):
+    poisk = deque()
+    poisk.append([st])
+    visited = set([st])
+    while poisk:
+        path = poisk.popleft()
+        eta_room = path[-1]
+        
+        if eta_room == end:
+            return path
+        
+        for sosedn in graf[eta_room]:
+            if sosedn not in visited:
+                visited.add(sosedn)
+                new_path = path + [sosedn]
+                poisk.append(new_path)
+    
+    return None
+
+# Ищем путь
+start_room = 'S'
+end_room = 'I'
+result = shortest(start_room, end_room)
+
+if result:
+    print("Кратчайший путь:")
+    print(" -> ".join(result))
+    print("Длина пути: " + str(len(result) - 1) + " шагов") #тут 4 решения, которые зависят от порядка заполнения графа
+else:
+    print("Путь не найден!")
